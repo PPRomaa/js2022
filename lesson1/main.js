835e76bfff7cb4b9d9a8a394b941e86d53ec2381
PPRomaa <popadinets.roman1@gmail.com>
2022-07-22 01:09:23 +0300

    Js-2022

---
+++ lesson1/main.js
@@ -0,0 +1,52 @@
+    let  str1 = 'hello';
+    let str2 = 'owu';
+    let str3 = 'com';
+    let str4 = 'ua';
+
+    let words1 = `${str1} ${str2} ${str3} ${str4}`;
+    alert(words1);
+
+
+    let n1 = 1;
+    console.log(n1);
+    n1 = 10;
+    console.log(n1);
+    n1 = -999;
+    console.log(n1);
+    n1 = 123;
+    console.log(n1);
+    n1 = 3.14;
+    console.log(n1);
+    n1 = 2.7;
+    console.log(n1);
+    n1 = 16;
+    console.log(n1);
+
+    let bool1 = true;
+    console.log(bool1);
+    let bool2 = false;
+    console.log(bool2);
+
+    let firstname = "Rodney";
+    let middlename = " Ryan";
+    let lastname = "Reynolds";
+    let person = `${firstname} ${middlename} ${lastname}`;
+    document.write(person);
+
+    let a = 100;
+    console.log(typeof a);
+    let b = '100';
+    console.log(typeof b);
+    let c = true;
+    console.log(typeof c);
+
+    let years = "2022";
+
+    let inputdata = prompt(middlename);
+    console.log(!!inputdata);
+
+    let inputdata2 = prompt(lastname);
+    console.log(!!inputdata2);
+
+    let inputdata3 = prompt(years);
+    console.log(!!inputdata3);
