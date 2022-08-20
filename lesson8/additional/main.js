f77d686df59a630b16a6237920fe33e59a943e2f
PPRomaa <popadinets.roman1@gmail.com>
2022-08-20 14:30:26 +0300

    Hw-8+additional

--- lesson8/additional/main.js
+++ lesson8/additional/main.js
@@ -1,11 +1,17 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
 
-
-function gcname (htmlElement) {
-    console.log(htmlElement)
-    let narray = [];
-    let slet = document.getElementsByClassName(`rules`);
-    narray.push(slet)
-    return console.log(narray)
-}
-gcname(document.body);
+// let array = [];
+// function foo (htmlElement){
+//     if (htmlElement.className){
+//         array.push(htmlElement)
+//     }
+//     let children = htmlElement;
+//     if (children.children.length !== 0){
+//         for (const child of children.children) {
+//             foo(child)
+//         }
+//     }
+// }
+//
+// foo(document.body)
+// console.log(array)
