d8c568d7d8be9c9807bf9c206565d05879978100
PPRomaa <popadinets.roman1@gmail.com>
2022-08-22 01:00:50 +0300

    Hw-8+additional

--- lesson8/task7/main.js
+++ lesson8/task7/main.js
@@ -22,8 +22,60 @@
 //     - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку
 // зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
 
+// let inputf = document.createElement(`form`)
+// inputf.id = 'form'
+//
+// let age=document.createElement('input');
+// age.name='age';
+// age.type='number';
+//
+//
+// let inputbtn = document.createElement("button")
+// inputbtn.innerText = 'send'
+//
+// inputf.addEventListener('submit',e =>{
+//     e.preventDefault()
+//     if (e.target.age.value < 18){
+//         alert('go away')
+//     }else {
+//         alert('hello')
+//     }
+// })
+//
+// inputf.append(age,inputbtn)
+// document.body.appendChild(inputf)
+
+
 
 
 // *** Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
 //     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
 // (Додатковачастина для завдання)
+
+// let first = document.createElement(`input`)
+// first.name = 'first'
+// let second = document.createElement(`input`)
+// second.name = 'second'
+// let third = document.createElement(`input`)
+// third.name = 'third'
+// let btn = document.createElement(`button`)
+// btn.innerText = 'commit'
+//
+// btn.onclick =()=>{
+//     let table = document.createElement(`table`)
+//     table.style.border = "2px solid black";
+//     for (let i = 0; i < first.value; i++) {
+//         let first = document.createElement(`tr`)
+//         first.style.border = "1px solid black";
+//         for (let j = 0; j < second.value; j++) {
+//             let second = document.createElement(`td`)
+//             third.style.border = "1px solid black";
+//             second.innerText = third.value
+//             first.appendChild(second)
+//             table.appendChild(first)
+//         }
+//     document.body.appendChild(table)
+//     }
+// }
+//
+// document.body.append(first,second,third,btn)
